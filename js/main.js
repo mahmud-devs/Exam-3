9786612c95f3a432136478f4a635b8548f91c6ac
mahmud-devs <mahmudulislam0999@gmail.com>
2024-01-30 21:08:19 +0600

    .

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
-
-
-for(let i = 0; i <= 20 ; i++){
+for (let i = 0; i <= 20; i++) {
     console.log(`${i} X 5 = tomar boyos ${i * 5} bosor `);
-    let res = i*5 ;
-    if(res < 20 && res >= 0 ){
+    let res = i * 5;
+    if (res < 20 && res >= 0) {
         console.log("Tumi biye Korte parbe na;");
-    }else if(res >= 20 && res <= 45){
+    } else if (res >= 20 && res <= 45) {
         console.log("Tumi tara tari biye koro;");
-    }else if(res > 45 && res <= 100){
+    } else if (res > 45 && res <= 100) {
         console.log("Tomar biye korar somoy shes;");
-    }
-    else{
+    } else {
         console.log("Invalid result");
     }
-    
 }
-
