d6923e24e2d704061552e1c4e8a02aacad5cb777
mahmud-devs <mahmudulislam0999@gmail.com>
2024-01-30 20:58:06 +0600

    Fixed

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 
 
 for(let i = 0; i <= 20 ; i++){
-    console.log(`${i} X 5 = tomar boyos ${i * 5}  `);
-    if(i < 4 && i >= 0 ){
-        console.log("Tumi biye Korte parbe na");
-    }else if(i >= 4 && i <= 9){
-        console.log("Tumi tara tari biye koro");
-    }else if(i > 9 && i <= 20){
-        console.log("Tomar biye korar somoy shes");
+    console.log(`${i} X 5 = tomar boyos ${i * 5} bosor `);
+    let res = i*5 ;
+    if(res < 20 && res >= 0 ){
+        console.log("Tumi biye Korte parbe na;");
+    }else if(res >= 20 && res <= 45){
+        console.log("Tumi tara tari biye koro;");
+    }else if(res > 45 && res <= 100){
+        console.log("Tomar biye korar somoy shes;");
     }
     else{
         console.log("Invalid result");
     }
+    
 }
 
